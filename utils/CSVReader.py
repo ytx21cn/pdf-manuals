04ecaf40415424c680df5e220a69e274ce9dd86a
ytx21cn <5900105+ytx21cn@users.noreply.github.com>
2020-11-10 09:43:15 -0800

    download all if newer: finally change back to start directory

--- utils/CSVReader.py
+++ utils/CSVReader.py
@@ -10,7 +10,12 @@ class CSVReader:
     field_names = [v.value for v in SourceKeys.__members__.values()]
 
     @staticmethod
-    def read_csv(csv_file: str):
+    def read_csv(csv_file: str) -> List[Dict]:
+        """
+        Read csv and return data.
+        :param csv_file: path to csv file.
+        :return: a list of rows, with keys being the field names of each column.
+        """
         with open(csv_file) as csv_obj:
             reader = csv.DictReader(csv_obj, fieldnames=CSVReader.field_names)
             data: List[Dict] = list(reader)
--- utils/wget.py
+++ utils/wget.py
@@ -1,6 +1,6 @@
 import subprocess as sp
 import os
-from os.path import join, relpath, dirname
+from os.path import join
 
 from utils.CSVReader import CSVReader
 from utils.sources_data_struct import pdf_base_dir, SourceKeys
@@ -16,19 +16,24 @@ class Wget:
 
     @staticmethod
     def download_all_if_newer(sources_csv: str):
-        data = CSVReader.read_csv(sources_csv)
 
-        for row in data:
-            # get information for download
-            category: str = row[SourceKeys.CATEGORY.value]
-            filename: str = row[SourceKeys.FILENAME.value]
-            url: str = row[SourceKeys.URL.value]
-            assert url.endswith(filename)
+        start_dir = os.getcwd()
 
-            # make category directory
-            target_dir = join(pdf_base_dir, category)
-            os.makedirs(target_dir, exist_ok=True)
+        try:
+            data = CSVReader.read_csv(sources_csv)
+            for row in data:
+                # get information for download
+                category: str = row[SourceKeys.CATEGORY.value]
+                filename: str = row[SourceKeys.FILENAME.value]
+                url: str = row[SourceKeys.URL.value]
+                assert url.endswith(filename)
 
-            # download pdf manual
-            os.chdir(target_dir)
-            Wget.download_newer_only(url)
+                # make category directory
+                target_dir = join(pdf_base_dir, category)
+                os.makedirs(target_dir, exist_ok=True)
+
+                # download pdf manual
+                os.chdir(target_dir)
+                Wget.download_newer_only(url)
+        finally:
+            os.chdir(start_dir)
